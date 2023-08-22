18eabb7b5bee0b66dae3c6d0a25b30495f3628bb
Paul Miller <paul.miller@rmit.edu.au>
2023-08-22 14:17:44 +1000

    removed and unnecessary fields and add comments / hints for students.

--- a2/pcb.h
+++ a2/pcb.h
@@ -8,17 +8,24 @@
 
 #pragma once
 class pcb {
+    // the unique process id
     osp2023::id_type id;
+    // the total time that a process / job should run for
     osp2023::time_type total_time;
+    // the time that has been used so far for this process
     osp2023::time_type time_used;
+    // the priority level for this process
     osp2023::priority_type priority;
+    // the last time this process was promoted
     osp2023::time_type time_since_promoted;
+    // how much of the time for this process has it spent being blocked?
     osp2023::time_type blocked_time;
-    osp2023::time_type total_blocked_time;
+    // how much time has this process spent waiting for the cpu?
     osp2023::time_type total_wait_time;
+    // what time was this process last on the cpu?
     osp2023::time_type last_on_cpu;
+    // what percentage of time should this process spend dealing with io?
     double io_fraction;
-    static osp2023::id_type next_id;
     friend std::ostream& operator<<(std::ostream&, const pcb&);
 
    public:
@@ -26,8 +33,6 @@ class pcb {
     constexpr static osp2023::priority_type lowest_priority() {
         return LOWEST_PRIORITY;
     }
-    static osp2023::id_type get_next_id() { return next_id++; }
     static constexpr osp2023::time_type MAX_DURATION = 5000;
     static constexpr osp2023::priority_type LOWEST_PRIORITY = 20;
-    static void set_next_id(osp2023::id_type theid) { next_id = theid; }
 };
